0c6c54ca86d6cbc781f21dc410aa50f23ebfd7c2
diasomirbayev <omirbaevdias2002@gmail.com>
2021-03-05 23:51:22 +0600

    second commit

--- src/Application.java
+++ src/Application.java
@@ -1,3 +1,65 @@
-public class Application {
+import java.sql.SQLException;
+import java.util.Scanner;
 
+public class Application {
+    static Scanner scanner = new Scanner(System.in);
+    static Database database = new Database();
+    //  git add .
+    //  git commit -m "second commit"
+    //  git push -u origin
+    public static void adminInterface() throws SQLException, ClassNotFoundException {
+        database.setConnection();
+        int number = 0;
+        while (number != 7) {
+            System.out.println("===================================");
+            System.out.println("Menu:");
+            System.out.println("1. Add student");
+            System.out.println("2. Add teacher");
+            System.out.println("3. Add subject");
+            System.out.println("4. Add student to teacher's student list");
+            System.out.println("5. Add subject to student's taken subjects");
+            System.out.println("6. Add subject to teacher's leading subjects");
+            System.out.println("7. Exit");
+            number = scanner.nextInt();
+            switch (number) {
+                case 1:
+                    database.addStudent();
+                    break;
+                case 2:
+                    database.addTeacher();
+                    break;
+                case 3:
+                    database.addSubject();
+                    break;
+                case 4:
+                    database.addStudentToTeacher();
+                    break;
+
+                case 5:
+                    database.addSubjectToStudent();
+                    break;
+                case 6:
+                    database.addSubjectToTeacher();
+                    break;
+                case 7:
+                    break;
+                default:
+                    System.out.println("Enter correct number!");
+                    break;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        System.out.println("Choose your type: admin(a)/teacher(t)/student(s)");
+        char c = scanner.next().charAt(0);
+        switch (c){
+            case 'a':
+                adminInterface();
+            case 't':
+//                teacherInterface();
+            case 's':
+//                studentInterface();
+        }
+    }
 }
--- src/Database.java
+++ src/Database.java
@@ -3,14 +3,15 @@ import classes.Subject;
 import classes.Teacher;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class Database {
     private Connection connection;
+    private Scanner scanner = new Scanner(System.in);
 
-    private Connection getConnection() throws ClassNotFoundException, SQLException {
+    public void setConnection() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
         connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/moodle", "root", "");
-        return connection;
     }
 
     public int getStudentId(String name) throws SQLException {
@@ -36,7 +37,14 @@ public class Database {
     }
 
 
-    public void addStudent(Student student) throws SQLException {
+    public void addStudent() throws SQLException {
+        System.out.println("Name:");
+        String name = scanner.next();
+        System.out.println("Email:");
+        String email = scanner.next();
+        System.out.println("Password:");
+        String password = scanner.next();
+        Student student = new Student(name, email, password);
         if (getStudentId(student.getName()) > 0) {
             return;
         }
@@ -45,10 +53,21 @@ public class Database {
         preparedStatement.setString(1, student.getName());
         preparedStatement.setString(2, student.getEmail());
         preparedStatement.setString(3, student.getPassword());
-        preparedStatement.executeUpdate();
+        if (preparedStatement.executeUpdate() > 0) {
+            System.out.println("Successfully added!");
+        } else {
+            System.out.println("Error occurred!");
+        }
     }
 
-    public void addTeacher(Teacher teacher) throws SQLException {
+    public void addTeacher() throws SQLException {
+        System.out.println("Name:");
+        String name = scanner.next();
+        System.out.println("Email:");
+        String email = scanner.next();
+        System.out.println("Password:");
+        String password = scanner.next();
+        Teacher teacher = new Teacher(name, email, password);
         if (getTeacherId(teacher.getName()) > 0) {
             return;
         }
@@ -60,7 +79,12 @@ public class Database {
         preparedStatement.executeUpdate();
     }
 
-    public void addSubject(Subject subject) throws SQLException {
+    public void addSubject() throws SQLException {
+        System.out.println("Name:");
+        String name = scanner.next();
+        System.out.println("Credit:");
+        int credit = scanner.nextInt();
+        Subject subject = new Subject(name, credit);
         String query = "insert into subject values(?,?)";
         PreparedStatement preparedStatement = connection.prepareStatement(query);
         preparedStatement.setString(1, subject.getName());
@@ -68,10 +92,14 @@ public class Database {
         preparedStatement.executeUpdate();
     }
 
-    public void addStudentToTeacher(Student student, Teacher teacher) throws SQLException {
-        int studentId = getStudentId(student.getName());
-        int teacherId = getTeacherId(teacher.getName());
-        if(studentId>0 && teacherId>0){
+    public void addStudentToTeacher() throws SQLException {
+        System.out.println("Teacher name:");
+        String teacherName = scanner.next();
+        System.out.println("Student name:");
+        String studentName = scanner.next();
+        int studentId = getStudentId(studentName);
+        int teacherId = getTeacherId(teacherName);
+        if (studentId > 0 && teacherId > 0) {
             String query = "insert into teacher_student_connection values(?,?)";
             PreparedStatement preparedStatement = connection.prepareStatement(query);
             preparedStatement.setInt(1, teacherId);
@@ -79,23 +107,33 @@ public class Database {
             preparedStatement.executeUpdate();
         }
     }
-    public void addSubjectToTeacher(Subject subject, Teacher teacher) throws SQLException {
-        int teacherId = getTeacherId(teacher.getName());
-        if(teacherId>0){
+
+    public void addSubjectToTeacher() throws SQLException {
+        System.out.println("Teacher name:");
+        String teacherName = scanner.next();
+        System.out.println("Subject name:");
+        String subjectName = scanner.next();
+        int teacherId = getTeacherId(teacherName);
+        if (teacherId > 0) {
             String query = "insert into teacher_subject_connection values(?,?)";
             PreparedStatement preparedStatement = connection.prepareStatement(query);
             preparedStatement.setInt(1, teacherId);
-            preparedStatement.setString(2, subject.getName());
+            preparedStatement.setString(2, subjectName);
             preparedStatement.executeUpdate();
         }
     }
-    public void addSubjectToStudent(Subject subject, Student student) throws SQLException {
-        int studentId = getStudentId(student.getName());
-        if(studentId>0){
+
+    public void addSubjectToStudent() throws SQLException {
+        System.out.println("Student name:");
+        String studentName = scanner.next();
+        System.out.println("Subject name:");
+        String subjectName = scanner.next();
+        int studentId = getStudentId(studentName);
+        if (studentId > 0) {
             String query = "insert into student_subject_connection values(?,?,?)";
             PreparedStatement preparedStatement = connection.prepareStatement(query);
             preparedStatement.setInt(1, studentId);
-            preparedStatement.setString(2, subject.getName());
+            preparedStatement.setString(2, subjectName);
             preparedStatement.setInt(3, -1);
             preparedStatement.executeUpdate();
         }
